e7205a13d070e0596cee2f91b0c2289749cd8a3b
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-11 00:01:31 -0500

    Convert visit_date to proper format

--- jobs/silver_cleaning.py
+++ jobs/silver_cleaning.py
@@ -4,4 +4,5 @@ from pyspark.sql.functions import col, to_date
 def clean_visit_data(df, silver_path):
 
     cleaned_df = df.dropDuplicates() \
-        .dropna()
+        .dropna() \
+        .withColumn("visit_date", to_date(col("visit_date")))
