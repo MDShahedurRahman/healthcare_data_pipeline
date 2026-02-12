530c713af609048a06f394cfe327e2db579a9298
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-11 23:40:04 -0500

    Integrate spark session in main()

---
+++ main.py
@@ -0,0 +1,17 @@
+from config import RAW_FILE, BRONZE_PATH, SILVER_PATH, GOLD_PATH
+from utils.spark_session import get_spark_session
+
+from jobs.bronze_ingestion import ingest_patient_visits
+from jobs.silver_cleaning import clean_visit_data
+from jobs.gold_star_schema import build_star_schema
+from jobs.business_queries import revenue_by_department, top_diagnosis, city_wise_cost
+
+
+def main():
+    spark = get_spark_session()
+
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
