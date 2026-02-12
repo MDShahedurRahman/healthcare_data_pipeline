ea8548debf36b16d77132bea0bd67aeab188354f
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-11 23:32:43 -0500

    Implement fact_visits creation

--- jobs/gold_star_schema.py
+++ jobs/gold_star_schema.py
@@ -8,4 +8,14 @@ def build_star_schema(df, gold_path):
         "doctor_id", "doctor_name", "department"
     ).distinct()
 
+    fact_visits = df.select(
+        "visit_id",
+        "patient_id",
+        "doctor_id",
+        "visit_date",
+        "visit_cost",
+        "diagnosis",
+        "hospital_city"
+    )
+
     return fact_visits
